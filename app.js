98f10074388e3d2bb6bcfcf0a262933b0817d0b4
Kyle Weintraub <Weintraub.Kyle@gmail.com>
2017-11-07 10:16:23 -0700

    small change to app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 
-const app = express();
-app.use(cors());
 
 var characters = [{
   id: 1,
@@ -38,6 +36,9 @@ function charByID(char, id) {
     }
   }
 }
+
+const app = express();
+app.use(cors());
 
 app.get("/", function (req, res) {
   response.json(characters);
